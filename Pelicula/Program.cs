640d875282b379343cd3f5d99a2d45f0d7ec769e
LuisRoldanC <124113829+LuisRoldanC@users.noreply.github.com>
2023-03-20 06:22:38 +0000

    Cuarta Version: Lista de peliculas

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -13,17 +13,16 @@ namespace Pelicula
         private string director;
 
       //  private List<Actor> actores = new List<Actor>();
-
         //Constructores
      
-        //Métodos
-   /* public Pelicula (string titulo, Int16 año, string pais, string director){
+      //Métodos
+  /*  public Pelicula (string titulo, Int16 año, string pais, string director){
         this.titulo = titulo;
         this.año = año;
         this.pais = pais;
         this.director = director;
-}
-*/
+}*/
+
         public void SetTitulo(string titulo)
         {
             this.titulo = titulo;
@@ -40,12 +39,14 @@ namespace Pelicula
             get { return titulo; }
             set { titulo = value; }
         }
-        public Int16? GetAño()
+        public Int16 GetAño()
         {
             return año;
         }
-        
-
+        public Int16 Año {
+            get { return año; }
+            set { año = value; }
+        }
         public Pelicula(string t, Int16 a){
             titulo = t;
             año = a;
@@ -81,6 +82,20 @@ namespace Pelicula
     {
         static void Main(string[] args)
         {
+       List<Pelicula> peliculas = new List<Pelicula>();
+
+        peliculas.Add(new Pelicula () {Titulo = "Titanic", Año = 1997});
+        peliculas.Add(new Pelicula () {Titulo = "Pinocho", Año = 2022});
+        peliculas.Add(new Pelicula () {Titulo = "Rapidos y furiosos 10", Año = 2023});   
+        peliculas.Add(new Pelicula () {Titulo = "Top Gun Maverick", Año = 2021});
+        peliculas.Add(new Pelicula () {Titulo = "El Rey Leon", Año = 2020});
+
+        foreach (Pelicula p in peliculas)
+        {
+            Console.WriteLine("{0} ({1})", p.Titulo, p.Año);
+        }
+
+        /*
         Pelicula p1 = new Pelicula();
         p1.SetTitulo("Titanic");
         p1.SetAño(1997);
@@ -90,7 +105,7 @@ namespace Pelicula
         p2.SetTitulo("Pinocho");
         p2.SetAño(2022);
         Console.WriteLine("{0}({1})", p2.GetTitulo(), p2.GetAño());
+       */
         }
     }
 }
-
